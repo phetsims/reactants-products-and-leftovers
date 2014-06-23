2a5d7bebe9bb21c5c3c32fce223a490d7018da43
Chris Malley <cmalley@pixelzoom.com>
2014-06-23 16:50:44 -0600

    programmatically create 'Molecules' screen icon

--- js/molecules/MoleculesScreen.js
+++ js/molecules/MoleculesScreen.js
@@ -9,23 +9,38 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var H2ONode = require( 'NITROGLYCERIN/nodes/H2ONode' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Image = require( 'SCENERY/nodes/Image' );
   var MoleculesModel = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/molecules/model/MoleculesModel' );
   var MoleculesView = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/molecules/view/MoleculesView' );
+  var Node = require( 'SCENERY/nodes/Node' );
   var RPALColors = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/common/RPALColors' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Screen = require( 'JOIST/SCREEN' );
 
   // strings
   var screenTitle = require( 'string!REACTANTS_PRODUCTS_AND_LEFTOVERS/screen.molecules' );
 
-  // images
-  //TODO create this programmatically
-  var screenImage = require( 'image!REACTANTS_PRODUCTS_AND_LEFTOVERS/Molecules-screen.png' );
+  // creates the icon for this screen
+  var createIcon = function() {
+
+     // background rectangle
+    var width = Screen.HOME_SCREEN_ICON_SIZE.width;
+    var height = Screen.HOME_SCREEN_ICON_SIZE.height;
+    var background = new Rectangle( 0, 0, width, height, { fill: 'white' } );
+
+    // H2O molecules, scaled to fit and centered on background
+    var moleculeNode = new H2ONode( { stroke: 'gray', lineWidth: 0.1 } );
+    moleculeNode.setScaleMagnitude( Math.min( 0.82 * background.width / moleculeNode.width, 0.82 * background.height / moleculeNode.height ) );
+    moleculeNode.center = background.center;
+
+    return new Node( { children: [ background, moleculeNode ] } );
+  };
   
   function MoleculesScreen() {
     Screen.call( this, screenTitle, 
-      new Image( screenImage ),
+      createIcon(),
       function() { return new MoleculesModel(); },
       function( model ) { return new MoleculesView( model ); },
       { backgroundColor: RPALColors.SCREEN_BACKGROUND }
