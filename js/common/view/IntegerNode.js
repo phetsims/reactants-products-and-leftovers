3e5f42dc430a4ffb47757dc464e97f927ca7dac0
Chris Malley <cmalley@pixelzoom.com>
2014-08-25 13:35:48 -0600

    IntegerNode

---
+++ js/common/view/IntegerNode.js
@@ -0,0 +1,25 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Displays a dynamic integer value.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Text = require( 'PATH/Text' );
+  var Util = require( 'DOT/Util' );
+
+  function IntegerNode( valueProperty, options ) {
+    Text.call( this, options );
+    valueProperty.link( function( value ) {
+      assert && assert( Util.isInteger( value ) );
+      value.text = '' + value;
+    } );
+  }
+
+  return inherit( Text, IntegerNode );
+} );
