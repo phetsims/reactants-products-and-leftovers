8addbb4dd437597a8e5ac260874edf9ba527bab1
Chris Malley <cmalley@pixelzoom.com>
2014-09-02 13:00:10 -0600

    doc

--- js/common/model/Product.js
+++ js/common/model/Product.js
@@ -1,8 +1,8 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
-//TODO eliminate this? it was a marker type in Java implementation
 /**
  * Chemical reactions yield one or more products, which have properties different from the reactants.
+ * This type adds no new functionality, it serves as a 'marker' type.
  * 
  * @author Chris Malley (PixelZoom, Inc.)
  */
