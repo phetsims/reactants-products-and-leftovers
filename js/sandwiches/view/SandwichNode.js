c25d1ea87436247616f5a958cc76fe1a77460855
Chris Malley <cmalley@pixelzoom.com>
2014-10-13 18:40:59 -0600

    lint fix

--- js/sandwiches/view/SandwichNode.js
+++ js/sandwiches/view/SandwichNode.js
@@ -9,7 +9,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var Dimension2 = require( 'DOT/Dimension2' );
   var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
