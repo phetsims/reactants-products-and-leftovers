d3622908388496db9ebec35e425632142cdbdb85
Chris Malley <cmalley@pixelzoom.com>
2014-10-20 16:25:00 -0600

    flesh out ResultsNode

--- js/game/view/ResultsNode.js
+++ js/game/view/ResultsNode.js
@@ -1,11 +1,21 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
+/**
+ * Portion of the scenegraph that corresponds to the 'results' game phase. (See GamePhase.RESULTS)
+ * Displays a panel with the game results.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
 define( function( require ) {
   'use strict';
 
   // modules
+  var GamePhase = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/game/model/GamePhase' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var LevelCompletedNode = require( 'VEGAS/LevelCompletedNode' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var RPALQueryParameters = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/common/RPALQueryParameters' );
+//TODO  var RPALRewardNode = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/game/view/RPALRewardNode' );
 
   /**
    * @param {GameModel} model
@@ -14,14 +24,60 @@ define( function( require ) {
    * @constructor
    */
   function ResultsNode( model, layoutBounds, audioPlayer ) {
-    Node.call( this );
-    //TODO
+
+    var thisNode = this;
+    Node.call( thisNode );
+
+    thisNode.rewardNode = null;
+
+    // show results when we enter this phase
+    model.gamePhaseProperty.link( function( gamePhase ) {
+      if ( gamePhase === GamePhase.RESULTS ) {
+
+        // game reward, shown for perfect score (or with 'reward' query parameter)
+        if ( model.isPerfectScore() || RPALQueryParameters.REWARD ) {
+
+          audioPlayer.gameOverPerfectScore();
+
+          //TODO
+//          thisNode.rewardNode = new RPALRewardNode( model.level );
+//          thisNode.addChild( thisNode.rewardNode );
+        }
+        else {
+          audioPlayer.gameOverImperfectScore();
+        }
+
+        // game results
+        thisNode.addChild( new LevelCompletedNode(
+          model.level,
+          model.score,
+          model.getPerfectScore(),
+            model.getPerfectScore() / model.maxPointsPerChallenge, // number of stars in the progress indicator
+          model.timerEnabled,
+          model.timer.elapsedTime,
+          model.bestTimeProperties[ model.level ].get(),
+          model.isNewBestTime,
+          function() {
+            model.gamePhaseProperty.set( GamePhase.SETTINGS );
+          }, {
+            starDiameter: 45,
+            buttonFill: LineGameConstants.BUTTON_COLOR,
+            centerX: layoutBounds.centerX,
+            centerY: layoutBounds.centerY
+          } ) );
+      }
+      else {
+        thisNode.removeAllChildren();
+        thisNode.rewardNode = null;
+      }
+    } );
   }
 
   return inherit( Node, ResultsNode, {
-
     step: function( elapsedTime ) {
-      //TODO
+      if ( this.rewardNode ) {
+        this.rewardNode.step( elapsedTime );
+      }
     }
   } );
 } );
