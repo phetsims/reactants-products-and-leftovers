084da7d7cbfd8232836fa9672605cec408a2ef96
Chris Malley <cmalley@pixelzoom.com>
2014-10-21 13:23:16 -0600

    fold IconFactory into SettingsNode, since all of its functions should be private to SettingsNode

--- js/game/view/SettingsNode.js
+++ js/game/view/SettingsNode.js
@@ -10,74 +10,38 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var ArrowNode = require( 'SCENERY_PHET/ArrowNode' );
   var GamePhase = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/game/model/GamePhase' );
-  var IconFactory = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/game/view/IconFactory' );
+  var H2ONode = require( 'NITROGLYCERIN/nodes/H2ONode' );
+  var HClNode = require( 'NITROGLYCERIN/nodes/HClNode' );
   var inherit = require( 'PHET_CORE/inherit' );
   var LayoutBox = require( 'SCENERY/nodes/LayoutBox' );
   var LevelSelectionButton = require( 'VEGAS/LevelSelectionButton' );
+  var NH3Node = require( 'NITROGLYCERIN/nodes/NH3Node' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   var RPALConstants = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/common/RPALConstants' );
   var RPALFont = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/common/RPALFont' );
   var SoundToggleButton = require( 'SCENERY_PHET/buttons/SoundToggleButton' );
+  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var Text = require( 'SCENERY/nodes/Text' );
   var TimerToggleButton = require( 'SCENERY_PHET/buttons/TimerToggleButton' );
   var VisibilityControl = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/game/view/VisibilityControl' );
 
   // strings
   var chooseYourLevelString = require( 'string!REACTANTS_PRODUCTS_AND_LEFTOVERS/chooseYourLevel' );
+  var pattern_Level_0 = require( 'string!REACTANTS_PRODUCTS_AND_LEFTOVERS/pattern_Level_0' );
+  var doubleQuestionMarkString = require( 'string!REACTANTS_PRODUCTS_AND_LEFTOVERS/doubleQuestionMark' );
+  var questionMarkString = require( 'string!REACTANTS_PRODUCTS_AND_LEFTOVERS/questionMark' );
 
   // constants
   var SCREEN_X_MARGIN = 40;
   var SCREEN_Y_MARGIN = 20;
-
-  // Icons for the level-selection buttons, indexed by level
-  var levelIcons = [
-    IconFactory.createLevelOneIcon(),
-    IconFactory.createLevelTwoIcon(),
-    IconFactory.createLevelThreeIcon()
-  ];
-  var MAX_ICON_WIDTH = _.max( levelIcons, function( icon ) { return icon.width; } ).width;
-  var MAX_ICON_HEIGHT = _.max( levelIcons, function( icon ) { return icon.height; } ).height;
-
-  /**
-   * Creates a level-selection button
-   * @param {number} level
-   * @param {GameModel} model
-   * @returns {Node}
-   */
-  var createLevelSelectionButton = function( level, model ) {
-
-    assert && assert( level >= 0 && level < levelIcons.length );
-
-    // make all icons the same size
-    var icon = levelIcons[ level ];
-    var rect = new Rectangle( 0, 0, MAX_ICON_WIDTH, MAX_ICON_HEIGHT, { center: icon.center } );
-    var content = new Node( {
-      children: [ rect, icon ]
-    } );
-
-    return new LevelSelectionButton(
-      content,
-      model.challengesPerGame,
-      function() {
-        model.level = level;
-        model.gamePhaseProperty.set( GamePhase.PLAY );
-      },
-      model.bestScoreProperties[ level ],
-      model.getPerfectScore(),
-      {
-        baseColor: 'rgb( 240, 255, 204 )',
-        buttonXMargin: 15,
-        buttonYMargin: 15,
-        buttonWidth: 150,
-        buttonHeight: 150,
-        iconToProgressIndicatorYSpace: 15,
-        bestTimeProperty: model.bestTimeProperties[ level ],
-        bestTimeVisibleProperty: model.timerEnabledProperty
-      } );
-  };
+  var LABEL_OPTIONS = { font: new RPALFont( 45 ) };
+  var QUESTION_MARK_OPTIONS = { font: new RPALFont( { size: 70, weight: 'bold' } ) };
+  var ARROW_OPTIONS = { headHeight: 20, headWidth: 20, tailWidth: 6 };
+  var MOLECULE_SCALE = 3;
 
   /**
    * @param {GameModel} model
@@ -92,10 +56,19 @@ define( function( require ) {
     // Title
     var title = new Text( chooseYourLevelString, { font: new RPALFont( 40 ) } );
 
+    // Icons for the level-selection buttons, indexed by level
+    var levelIcons = [
+      createLevelOneIcon(),
+      createLevelTwoIcon(),
+      createLevelThreeIcon()
+    ];
+    var maxIconWidth = _.max( levelIcons, function( icon ) { return icon.width; } ).width;
+    var maxIconHeight = _.max( levelIcons, function( icon ) { return icon.height; } ).height;
+
     // Level-selection buttons, arranged in a row
     var buttons = [];
     for ( var level = 0; level < model.numberOfLevels; level++ ) {
-      buttons.push( createLevelSelectionButton( level, model ) );
+      buttons.push( createLevelSelectionButton( level, model, levelIcons[level], maxIconWidth, maxIconHeight ) );
     }
     var buttonsParent = new LayoutBox( {
       children: buttons,
@@ -113,7 +86,7 @@ define( function( require ) {
     var visibilityControl = new VisibilityControl( model.moleculesVisibleProperty, model.numbersVisibleProperty, {
       centerX: layoutBounds.centerX,
       bottom: layoutBounds.bottom - SCREEN_Y_MARGIN
-    });
+    } );
 
     // Reset All button, at rightBottom
     var resetAllButton = new ResetAllButton( {
@@ -147,6 +120,90 @@ define( function( require ) {
     ];
     Node.call( this, options );
   }
+
+  /**
+   * Creates a level-selection button
+   * @param {number} level
+   * @param {GameModel} model
+   * @param {Node} icon
+   * @param {number} maxIconWidth
+   * @param {number} maxIconHeight
+   * @returns {Node}
+   */
+  var createLevelSelectionButton = function( level, model, icon, maxIconWidth, maxIconHeight ) {
+
+    assert && assert( level >= 0 && level < levelIcons.length );
+
+    // make all icons the same size
+    var rect = new Rectangle( 0, 0, maxIconWidth, maxIconHeight, { center: icon.center } );
+    var content = new Node( {
+      children: [ rect, icon ]
+    } );
+
+    return new LevelSelectionButton(
+      content,
+      model.challengesPerGame,
+      function() {
+        model.level = level;
+        model.gamePhaseProperty.set( GamePhase.PLAY );
+      },
+      model.bestScoreProperties[ level ],
+      model.getPerfectScore(),
+      {
+        baseColor: 'rgb( 240, 255, 204 )',
+        buttonXMargin: 15,
+        buttonYMargin: 15,
+        buttonWidth: 150,
+        buttonHeight: 150,
+        iconToProgressIndicatorYSpace: 15,
+        bestTimeProperty: model.bestTimeProperties[ level ],
+        bestTimeVisibleProperty: model.timerEnabledProperty
+      } );
+  };
+
+  /*
+   *  Level N
+   *  leftNode -> rightNode
+   */
+  var createIcon = function( level, leftNode, rightNode ) {
+    var labelNode = new Text( StringUtils.format( pattern_Level_0, level ), LABEL_OPTIONS );
+    var arrowNode = new ArrowNode( 0, 0, 50, 0, ARROW_OPTIONS );
+    var icon = new LayoutBox( { children: [ leftNode, arrowNode, rightNode ], orientation: 'horizontal', spacing: 20 } );
+    return new LayoutBox( { children: [ labelNode, icon ], orientation: 'vertical', spacing: 30 } );
+  };
+
+  /**
+   *  Level 1
+   *  ? -> HCl
+   */
+  var createLevelOneIcon = function() {
+    var leftNode = new Text( questionMarkString, QUESTION_MARK_OPTIONS );
+    var rightNode = new HClNode( RPALConstants.ATOM_OPTIONS );
+    rightNode.setScaleMagnitude( MOLECULE_SCALE );
+    return createIcon( 1, leftNode, rightNode );
+  };
+
+  /**
+   *  Level 2
+   *  H2O -> ?
+   */
+  var createLevelTwoIcon = function() {
+    var leftNode = new H2ONode( RPALConstants.ATOM_OPTIONS );
+    leftNode.setScaleMagnitude( MOLECULE_SCALE );
+    var rightNode = new Text( questionMarkString, QUESTION_MARK_OPTIONS );
+    return createIcon( 2, leftNode, rightNode );
+  };
+
+  /**
+   *  Level 3
+   *  NH3 -> ??
+   */
+  var createLevelThreeIcon = function() {
+    var leftNode = new NH3Node( RPALConstants.ATOM_OPTIONS );
+    leftNode.setScaleMagnitude( MOLECULE_SCALE );
+    var rightNode = new Text( doubleQuestionMarkString, QUESTION_MARK_OPTIONS );
+    return createIcon( 3, leftNode, rightNode );
+  };
 
   return inherit( Node, SettingsNode );
 } );
