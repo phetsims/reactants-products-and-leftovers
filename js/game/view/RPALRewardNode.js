413d464f4f484cab46df03789cf4d6457323add1
Chris Malley <cmalley@pixelzoom.com>
2014-10-23 11:41:48 -0600

    put the correct icons with the correct level, create smiley faces with multiple colors, doc

--- js/game/view/RPALRewardNode.js
+++ js/game/view/RPALRewardNode.js
@@ -70,27 +70,32 @@ define( function( require ) {
     RewardNode.call( this, { nodes: nodeFactoryFunctions[level]() } );
   }
 
-  // Level 1 = molecules
+  // Level 1: molecules
   var createNodesLevel1 = function() {
-    var nodes = [ new FaceNode( 40 ) ];
-    return RewardNode.createRandomNodes( nodes, NUMBER_OF_NODES );
-  };
-
-  // Level 2 = smiley faces
-  var createNodesLevel2 = function() {
     var nodes = [
-      new CNode(), new C2H2Node(), new C2H4Node(), new C2H5ClNode(), new C2H5OHNode(), new C2H6Node(),
-      new CH2ONode(), new CH3OHNode(), new CH4Node(), new Cl2Node(), new CONode(), new CO2Node(), new CS2Node(),
-      new F2Node(), new H2Node(), new H2ONode(), new H2SNode(), new HClNode(), new HFNode(), new N2Node(),
-      new N2ONode(), new NH3Node(), new NONode(), new NO2Node(), new O2Node(), new OF2Node(), new P4Node(),
-      new PCl3Node(), new PCl5Node(), new PF3Node(), new PH3Node(), new SNode(), new SO2Node(), new SO3Node()
-    ];
+          new CNode(), new C2H2Node(), new C2H4Node(), new C2H5ClNode(), new C2H5OHNode(), new C2H6Node(),
+          new CH2ONode(), new CH3OHNode(), new CH4Node(), new Cl2Node(), new CONode(), new CO2Node(), new CS2Node(),
+          new F2Node(), new H2Node(), new H2ONode(), new H2SNode(), new HClNode(), new HFNode(), new N2Node(),
+          new N2ONode(), new NH3Node(), new NONode(), new NO2Node(), new O2Node(), new OF2Node(), new P4Node(),
+          new PCl3Node(), new PCl5Node(), new PF3Node(), new PH3Node(), new SNode(), new SO2Node(), new SO3Node()
+        ];
     return RewardNode.createRandomNodes( nodes, NUMBER_OF_NODES );
   };
 
-  // Level 3 = sandwiches
+  // Level 2: smiley faces (various colors)
+  var createNodesLevel2 = function() {
+    var nodes = [];
+    var colors = [ 'yellow', 'rgb(255,85,0)', 'orange', 'magenta', 'cyan', 'rgb(100,255,100)' ];
+    colors.forEach( function( color ) {
+        nodes.push( new FaceNode( 40, { headFill: color }  ) );
+    } );
+    return RewardNode.createRandomNodes( nodes, NUMBER_OF_NODES );
+  };
+
+  // Level 3: sandwiches
   var createNodesLevel3 = function() {
     var nodes = [
+      // args: bread, meat, cheese
       new SandwichNode( 3, 3, 3 ),
       new SandwichNode( 2, 1, 2 ),
       new SandwichNode( 1, 1, 1 ),
