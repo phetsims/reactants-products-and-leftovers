db6c45dc042944db9dc6ede0886c1665b9cc24e4
Chris Malley <cmalley@pixelzoom.com>
2014-11-01 12:28:16 -0600

    simplify creation of RPALSymbols

--- js/common/RPALSymbols.js
+++ js/common/RPALSymbols.js
@@ -9,44 +9,20 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var toSubscript = require( 'NITROGLYCERIN/ChemUtils' ).toSubscript;
+  var ChemUtils = require( 'NITROGLYCERIN/ChemUtils' );
 
-  var RPALSymbols = {
-    C: toSubscript( 'C' ),
-    C2H2: toSubscript( 'C2H2' ),
-    C2H4: toSubscript( 'C2H4' ),
-    C2H5Cl: toSubscript( 'C2H5Cl' ),
-    C2H5OH: toSubscript( 'C2H5OH' ),
-    C2H6: toSubscript( 'C2H6' ),
-    CH2O: toSubscript( 'CH2O' ),
-    CH3OH: toSubscript( 'CH3OH' ),
-    CH4: toSubscript( 'CH4' ),
-    Cl2: toSubscript( 'Cl2' ),
-    CO: toSubscript( 'CO' ),
-    CO2: toSubscript( 'CO2' ),
-    CS2: toSubscript( 'CS2' ),
-    F2: toSubscript( 'F2' ),
-    H2: toSubscript( 'H2' ),
-    H2O: toSubscript( 'H2O' ),
-    H2S: toSubscript( 'H2S' ),
-    HCl: toSubscript( 'HCl' ),
-    HF: toSubscript( 'HF' ),
-    N2: toSubscript( 'N2' ),
-    N2O: toSubscript( 'N2O' ),
-    NH3: toSubscript( 'NH3' ),
-    NO: toSubscript( 'NO' ),
-    NO2: toSubscript( 'NO2' ),
-    O2: toSubscript( 'O2' ),
-    OF2: toSubscript( 'OF2' ),
-    P4: toSubscript( 'P4' ),
-    PCl3: toSubscript( 'PCl3' ),
-    PCl5: toSubscript( 'PCl5' ),
-    PF3: toSubscript( 'PF3' ),
-    PH3: toSubscript( 'PH3' ),
-    S: toSubscript( 'S' ),
-    SO2: toSubscript( 'SO2' ),
-    SO3: toSubscript( 'SO3' )
-  };
+  // Plain-text versions of the symbols
+  var SYMBOLS = [
+    'C', 'C2H2', 'C2H4', 'C2H5Cl', 'C2H5OH', 'C2H6', 'CH2O', 'CH3OH', 'CH4', 'Cl2', 'CO', 'CO2', 'CS2',
+    'F2', 'H2', 'H2O', 'H2S', 'HCl', 'HF', 'N2', 'N2O', 'NH3', 'NO', 'NO2', 'O2', 'OF2',
+    'P4', 'PCl3', 'PCl5', 'PF3', 'PH3', 'S', 'SO2', 'SO3'
+  ];
+
+  // Create an object that has property names that match the plain-text symbols, values are HTML fragments.
+  var RPALSymbols = {};
+  SYMBOLS.forEach( function( symbol ) {
+    RPALSymbols[symbol] = ChemUtils.toSubscript( symbol );
+  } );
 
   return RPALSymbols;
 } );
