a7fc34e5150fa7c261429fb3c4ebf7965530732a
Chris Malley <cmalley@pixelzoom.com>
2014-11-01 11:57:07 -0600

    simplify layout of Game icon by using LayoutBox

--- js/game/GameScreen.js
+++ js/game/GameScreen.js
@@ -13,6 +13,7 @@ define( function( require ) {
   var GameModel = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/game/model/GameModel' );
   var GameView = require( 'REACTANTS_PRODUCTS_AND_LEFTOVERS/game/view/GameView' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var LayoutBox = require( 'SCENERY/nodes/LayoutBox' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
@@ -26,33 +27,34 @@ define( function( require ) {
   // creates the {Node} icon for this screen
   var createIcon = function() {
 
-    // constants
-    var faceDiameter = 200;
-    var arrowXSpacing = 25;
-    var arrowYSpacing = 10;
-
     // background rectangle
-    var width = Screen.HOME_SCREEN_ICON_SIZE.width;
-    var height = Screen.HOME_SCREEN_ICON_SIZE.height;
-    var background = new Rectangle( 0, 0, width, height, { fill: 'white' } );
+    var background = new Rectangle( 0, 0, Screen.HOME_SCREEN_ICON_SIZE.width, Screen.HOME_SCREEN_ICON_SIZE.height,
+      { fill: 'white' } );
 
     // face
-    var faceNode = new FaceNode( faceDiameter, { headStroke: 'black', headLineWidth: 4 } );
+    var faceNode = new FaceNode( 200, { headStroke: 'black', headLineWidth: 4 } );
 
     // up/down arrows
-    var arrowOptions = { fill: 'black' };
-    var arrowSize = 0.4 * ( faceNode.height - arrowYSpacing );
-    var upArrowNode = new Path( new Shape().moveTo( 0, 0 ).lineTo( arrowSize / 2, arrowSize ).lineTo( -arrowSize / 2, arrowSize ).close(), arrowOptions );
-    var downArrowNode = new Path( new Shape().moveTo( 0, 0 ).lineTo( arrowSize / 2, -arrowSize ).lineTo( -arrowSize / 2, -arrowSize ).close(), arrowOptions );
-
-    // layout, arrows to left of face
-    upArrowNode.right = faceNode.left - arrowXSpacing;
-    upArrowNode.bottom = faceNode.centerY - arrowYSpacing;
-    downArrowNode.right = faceNode.left - arrowXSpacing;
-    downArrowNode.top = faceNode.centerY + arrowYSpacing;
+    var ARROW_OPTIONS = { fill: 'black' };
+    var ARROW_SIZE = 0.4 * faceNode.height;
+    var upArrowNode = new Path( new Shape()
+        // clockwise from tip
+        .moveTo( 0, 0 )
+        .lineTo( ARROW_SIZE / 2, ARROW_SIZE )
+        .lineTo( -ARROW_SIZE / 2, ARROW_SIZE )
+        .close(),
+      ARROW_OPTIONS );
+    var downArrowNode = new Path( new Shape()
+        // clockwise from tip
+        .moveTo( 0, 0 )
+        .lineTo( -ARROW_SIZE / 2, -ARROW_SIZE )
+        .lineTo( ARROW_SIZE / 2, -ARROW_SIZE )
+        .close(),
+      ARROW_OPTIONS );
+    var arrowsBox = new LayoutBox( { children: [ upArrowNode, downArrowNode ], orientation: 'vertical', spacing: 20 } );
 
     // scale to fit, center in background
-    var contentNode = new Node( { children: [ faceNode, upArrowNode, downArrowNode ] } );
+    var contentNode = new LayoutBox( { children: [ arrowsBox, faceNode ], orientation: 'horizontal', spacing: 25 } );
     contentNode.setScaleMagnitude( Math.min( 0.82 * background.width / contentNode.width, 0.82 * background.height / contentNode.height ) );
     contentNode.center = background.center;
     return new Node( { children: [ background, contentNode ] } );
