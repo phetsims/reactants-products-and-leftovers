a42c6da08d54a7312a4a613d93126bcd14ae1061
Chris Malley <cmalley@pixelzoom.com>
2014-11-17 10:03:56 -0700

    doc

--- js/common/view/StacksAccordionBox.js
+++ js/common/view/StacksAccordionBox.js
@@ -1,7 +1,7 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
 /**
- *  Accordion box that shows stacks of items.
+ *  Accordion box that shows stacks of items, used in the 'Sandwiches' and 'Molecules' screens.
  *
  *  @author Chris Malley (PixelZoom, Inc.)
  */
