944c26320c05fa3912d7990b2ae781f9e34d544b
Chris Malley <cmalley@pixelzoom.com>
2014-11-24 13:44:04 -0700

    change request: use a thicker stroke on molecule in the navbar icon for 'Molecules' screen

--- js/molecules/MoleculesScreen.js
+++ js/molecules/MoleculesScreen.js
@@ -23,18 +23,24 @@ define( function( require ) {
 
   function MoleculesScreen() {
     Screen.call( this, screenTitle,
-      createIcon(),
+      createIcon( 0.1 ),
       function() { return new MoleculesModel(); },
       function( model ) { return new MoleculesView( model ); },
-      { backgroundColor: RPALColors.SCREEN_BACKGROUND }
+      {
+        backgroundColor: RPALColors.SCREEN_BACKGROUND,
+        navigationBarIcon: createIcon( 0.5 )
+      }
     );
   }
 
   /**
    * Creates the icon for this screen, an H2O molecule.
+   * @param {number} moleculeLineWidth lineWidth used to stroke the molecule icon
    * @returns {Node}
    */
-  var createIcon = function() {
+  var createIcon = function( moleculeLineWidth ) {
+
+    moleculeLineWidth = moleculeLineWidth || 1;
 
     // background rectangle
     var width = Screen.HOME_SCREEN_ICON_SIZE.width;
@@ -42,7 +48,7 @@ define( function( require ) {
     var background = new Rectangle( 0, 0, width, height, { fill: 'white' } );
 
     // H2O molecule, scaled to fit and centered on background
-    var moleculeNode = new H2ONode( { stroke: 'gray', lineWidth: 0.1 } );
+    var moleculeNode = new H2ONode( { atomOptions: { stroke: 'black', lineWidth: moleculeLineWidth } } );
     moleculeNode.setScaleMagnitude(
       Math.min( 0.82 * background.width / moleculeNode.width, 0.82 * background.height / moleculeNode.height ) );
     moleculeNode.center = background.center;
