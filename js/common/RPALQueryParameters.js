03b6ee721055bf0a7cc33a406b7ba6662cff6308
Chris Malley <cmalley@pixelzoom.com>
2014-12-09 12:32:48 -0700

    use !!getQueryParameter for boolean query parameters

--- js/common/RPALQueryParameters.js
+++ js/common/RPALQueryParameters.js
@@ -13,16 +13,16 @@ define( function( require ) {
   var RPALQueryParameters = {
 
     // adds 'Test', 'Skip', 'Replay' buttons to Game, shows challenge answers at the bottom of the Game screen
-    DEV: getQueryParameter( 'dev' ) || false,
+    DEV: !!getQueryParameter( 'dev' ) || false,
 
     // plays all reactions for each level of the game
-    PLAY_ALL: getQueryParameter( 'playAll' ) || false,
+    PLAY_ALL: !!getQueryParameter( 'playAll' ) || false,
 
     // shows the game reward regardless of score
-    REWARD: getQueryParameter( 'reward' ) || false,
+    REWARD: !!getQueryParameter( 'reward' ) || false,
 
     // shows challenge answers at the bottom of the Game screen
-    CHEAT: getQueryParameter( 'cheat' ) || false,
+    CHEAT: !!getQueryParameter( 'cheat' ) || false,
 
     //TODO delete when #20 is closed
     RANDOM_OFFSET: parseInt( getQueryParameter( 'randomOffset' ), 10 ) || 8,
