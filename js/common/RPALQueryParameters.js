d936a4f7c8ba1ca3a36a60320d082cd0a6e3bbc0
Chris Malley <cmalley@pixelzoom.com>
2014-12-09 12:30:31 -0700

    TODO

--- js/common/RPALQueryParameters.js
+++ js/common/RPALQueryParameters.js
@@ -24,18 +24,13 @@ define( function( require ) {
     // shows challenge answers at the bottom of the Game screen
     CHEAT: getQueryParameter( 'cheat' ) || false,
 
-    /**
-     * Molecules in game boxes are arranged in a grid. This controls how much the molecules are randomly offset from the center
-     * of cells in the grid. Higher values make the layout look less grid-like, but result in more overlap of molecules (a trade-off).
-     */
+    //TODO delete when #20 is closed
     RANDOM_OFFSET: parseInt( getQueryParameter( 'randomOffset' ), 10 ) || 8,
 
     //TODO delete when #22 is closed
-    // opacity of buttons in Game, 0 (invisible) to 1 (opaque)
     BUTTONS_OPACITY: parseFloat( getQueryParameter( 'buttonsOpacity' ), 10 ) || 0.75,
 
     //TODO delete when #22 is closed
-    // opacity of face in Game, 0 (invisible) to 1 (opaque)
     FACE_OPACITY: parseFloat( getQueryParameter( 'faceOpacity' ), 10 ) || 0.65
   };
 
