08f8ea489825850fd0c74d3fc23d2a10ccbe58ff
samreid <reids@colorado.edu>
2018-11-19 12:55:03 -0700

    Update copyright dates, see https://github.com/phetsims/chipper/issues/711

--- js/game/view/VisibilityRadioButtons.js
+++ js/game/view/VisibilityRadioButtons.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * Radio buttons for selecting what's visible/hidden in Game challenges.
