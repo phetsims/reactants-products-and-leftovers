44ec3c79bc33f848aae43264d9289904f1464854
samreid <reids@colorado.edu>
2020-02-27 12:07:55 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/reactants-products-and-leftovers-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import reactantsProductsAndLeftovers from './reactantsProductsAndLeftovers.js';
+
+const reactantsProductsAndLeftoversStrings = getStringModule( 'REACTANTS_PRODUCTS_AND_LEFTOVERS' );
+
+reactantsProductsAndLeftovers.register( 'reactantsProductsAndLeftoversStrings', reactantsProductsAndLeftoversStrings );
+
+export default reactantsProductsAndLeftoversStrings;
