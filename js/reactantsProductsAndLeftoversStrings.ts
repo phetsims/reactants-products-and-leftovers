2e1577b47b998cc2837a563e9f2cedc903325f16
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:22 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/reactantsProductsAndLeftoversStrings.ts
@@ -0,0 +1,46 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import reactantsProductsAndLeftovers from './reactantsProductsAndLeftovers.js';
+
+type StringsType = {
+  'reactants-products-and-leftovers': {
+    'title': string
+  },
+  'screen': {
+    'sandwiches': string,
+    'molecules': string,
+    'game': string
+  },
+  'reactants': string,
+  'products': string,
+  'leftovers': string,
+  'showAll': string,
+  'hideMolecules': string,
+  'hideNumbers': string,
+  'cheese': string,
+  'meatAndCheese': string,
+  'custom': string,
+  'makeWater': string,
+  'makeAmmonia': string,
+  'combustMethane': string,
+  'beforeReaction': string,
+  'afterReaction': string,
+  'beforeSandwich': string,
+  'afterSandwich': string,
+  'noReaction': string,
+  'chooseYourLevel': string,
+  'pattern_Level_0': string,
+  'questionMark': string,
+  'doubleQuestionMark': string
+};
+
+const reactantsProductsAndLeftoversStrings = getStringModule( 'REACTANTS_PRODUCTS_AND_LEFTOVERS' ) as StringsType;
+
+reactantsProductsAndLeftovers.register( 'reactantsProductsAndLeftoversStrings', reactantsProductsAndLeftoversStrings );
+
+export default reactantsProductsAndLeftoversStrings;
