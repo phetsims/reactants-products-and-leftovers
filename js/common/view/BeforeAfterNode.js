d6e9e8c004635d3f70597e4fa30cff926d6b21a1
phet-dev <phetadmin@gmail.com>
2022-06-01 03:41:00 -0600

    update copyright dates from daily grunt work

--- js/common/view/BeforeAfterNode.js
+++ js/common/view/BeforeAfterNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * This is the primary UI component for the 'Sandwiches' and 'Molecules' screens.
