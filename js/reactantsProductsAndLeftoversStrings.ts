1938761a344d98c63a19d65896754e217871f441
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:32 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/reactantsProductsAndLeftoversStrings.ts
+++ js/reactantsProductsAndLeftoversStrings.ts
@@ -5,38 +5,64 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import reactantsProductsAndLeftovers from './reactantsProductsAndLeftovers.js';
 
 type StringsType = {
   'reactants-products-and-leftovers': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'sandwiches': string;
+    'sandwichesProperty': TReadOnlyProperty<string>;
     'molecules': string;
+    'moleculesProperty': TReadOnlyProperty<string>;
     'game': string;
+    'gameProperty': TReadOnlyProperty<string>;
   };
   'reactants': string;
+  'reactantsProperty': TReadOnlyProperty<string>;
   'products': string;
+  'productsProperty': TReadOnlyProperty<string>;
   'leftovers': string;
+  'leftoversProperty': TReadOnlyProperty<string>;
   'showAll': string;
+  'showAllProperty': TReadOnlyProperty<string>;
   'hideMolecules': string;
+  'hideMoleculesProperty': TReadOnlyProperty<string>;
   'hideNumbers': string;
+  'hideNumbersProperty': TReadOnlyProperty<string>;
   'cheese': string;
+  'cheeseProperty': TReadOnlyProperty<string>;
   'meatAndCheese': string;
+  'meatAndCheeseProperty': TReadOnlyProperty<string>;
   'custom': string;
+  'customProperty': TReadOnlyProperty<string>;
   'makeWater': string;
+  'makeWaterProperty': TReadOnlyProperty<string>;
   'makeAmmonia': string;
+  'makeAmmoniaProperty': TReadOnlyProperty<string>;
   'combustMethane': string;
+  'combustMethaneProperty': TReadOnlyProperty<string>;
   'beforeReaction': string;
+  'beforeReactionProperty': TReadOnlyProperty<string>;
   'afterReaction': string;
+  'afterReactionProperty': TReadOnlyProperty<string>;
   'beforeSandwich': string;
+  'beforeSandwichProperty': TReadOnlyProperty<string>;
   'afterSandwich': string;
+  'afterSandwichProperty': TReadOnlyProperty<string>;
   'noReaction': string;
+  'noReactionProperty': TReadOnlyProperty<string>;
   'chooseYourLevel': string;
+  'chooseYourLevelProperty': TReadOnlyProperty<string>;
   'pattern_Level_0': string;
+  'pattern_Level_0Property': TReadOnlyProperty<string>;
   'questionMark': string;
+  'questionMarkProperty': TReadOnlyProperty<string>;
   'doubleQuestionMark': string;
+  'doubleQuestionMarkProperty': TReadOnlyProperty<string>;
 };
 
 const reactantsProductsAndLeftoversStrings = getStringModule( 'REACTANTS_PRODUCTS_AND_LEFTOVERS' ) as StringsType;
