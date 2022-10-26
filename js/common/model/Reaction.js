3b9b8c95499aad39ce24c82aeb1bc96e6b0d8f0f
phet-dev <phetadmin@gmail.com>
2022-10-26 03:57:05 -0600

    update copyright dates from daily grunt work

--- js/common/model/Reaction.js
+++ js/common/model/Reaction.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * A chemical reaction is a process that leads to the transformation of one set of
