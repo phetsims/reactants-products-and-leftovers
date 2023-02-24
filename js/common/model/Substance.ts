874fd7f251dd4816c325c11478ffab20c3f77343
pixelzoom <cmalley@pixelzoom.com>
2023-02-24 09:58:39 -0700

    convert to TypeScript, https://github.com/phetsims/reactants-products-and-leftovers/issues/80

--- js/common/model/Substance.ts
+++ js/common/model/Substance.ts
@@ -1,85 +1,75 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * A substance is a participant in a chemical reaction.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import { Node } from '../../../../scenery/js/imports.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import reactantsProductsAndLeftovers from '../../reactantsProductsAndLeftovers.js';
 
 export default class Substance {
 
-  /**
-   * @param {number} coefficient substance's coefficient in the reaction equation
-   * @param {string} symbol used in reaction equation
-   * @param {Node} icon visual representation of the substance
-   * @param {number} [quantity] how much of a substance we have, defaults to zero
-   */
-  constructor( coefficient, symbol, icon, quantity ) {
+  public readonly symbol: string;
 
-    quantity = quantity || 0;
+  // substance's coefficient in the reaction equation, mutable to support 'Custom' sandwich
+  public readonly coefficientProperty: Property<number>;
+
+  // how much of the substance we have
+  public readonly quantityProperty: Property<number>;
+
+  // visual representation of the substance, mutable to support the 'Custom' sandwich
+  public readonly iconProperty: Property<Node>;
+
+  /**
+   * @param coefficient - substance's coefficient in the reaction equation
+   * @param symbol - used in reaction equation
+   * @param icon - visual representation of the substance
+   * @param [quantity] - how much of a substance we have, defaults to zero
+   */
+  public constructor( coefficient: number, symbol: string, icon: Node, quantity = 0 ) {
 
     assert && assert( coefficient >= 0 );
     assert && assert( quantity >= 0 );
 
-    this.symbol = symbol; // @public {string}
+    this.symbol = symbol;
 
-    // @public {number} substance's coefficient in the reaction equation, mutable to support 'Custom' sandwich
     this.coefficientProperty = new NumberProperty( coefficient );
-
-    // @public {Node} visual representation of the substance, mutable to support the 'Custom' sandwich
-    this.iconProperty = new Property( icon );
-
-    // @public {number} how much of the substance we have
     this.quantityProperty = new NumberProperty( quantity );
+    this.iconProperty = new Property( icon );
   }
 
-  // @public
-  reset() {
+  public reset(): void {
     this.coefficientProperty.reset();
-    this.iconProperty.reset();
     this.quantityProperty.reset();
+    this.iconProperty.reset();
   }
 
   /*
    * Are 2 substances the same? AXON.Property observers are not considered.
-   * @param {Substance} substance
-   * @returns {boolean}
-   * @public
    */
-  equals( substance ) {
-    return ( substance instanceof Substance &&
-             this.symbol === substance.symbol &&
-             this.coefficientProperty.get() === substance.coefficientProperty.get() &&
-             this.iconProperty.get() === substance.iconProperty.get() &&
-             this.quantityProperty.get() === substance.quantityProperty.get() );
+  public equals( substance: Substance ): boolean {
+    return ( this.symbol === substance.symbol &&
+             this.coefficientProperty.value === substance.coefficientProperty.value &&
+             this.iconProperty.value === substance.iconProperty.value &&
+             this.quantityProperty.value === substance.quantityProperty.value );
   }
 
   /**
    * Creates a shallow copy of a substance. AXON.Property observers are not copied.
-   * @param {Substance} substance
-   * @returns {Substance}
-   * @static
-   * @public
    */
-  static clone( substance ) {
-    return new Substance( substance.coefficientProperty.get(), substance.symbol, substance.iconProperty.get(), substance.quantityProperty.get() );
+  public static clone( substance: Substance ): Substance {
+    return new Substance( substance.coefficientProperty.value, substance.symbol, substance.iconProperty.value, substance.quantityProperty.value );
   }
 
   /**
    * Creates a shallow copy of a substance with a specified quantity. AXON.Property observers are not copied.
-   * @param {Substance} substance
-   * @param {number} quantity
-   * @returns {Substance}
-   * @static
-   * @public
    */
-  static withQuantity( substance, quantity ) {
-    return new Substance( substance.coefficientProperty.get(), substance.symbol, substance.iconProperty.get(), quantity );
+  public static withQuantity( substance: Substance, quantity: number ): Substance {
+    return new Substance( substance.coefficientProperty.value, substance.symbol, substance.iconProperty.value, quantity );
   }
 }
 
