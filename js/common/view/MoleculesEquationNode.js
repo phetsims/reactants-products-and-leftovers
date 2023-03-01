4f0e480a812e5bbfd8318956da5a957863eaf841
pixelzoom <cmalley@pixelzoom.com>
2023-03-01 12:56:44 -0700

    // @ts-nocheck

--- js/common/view/MoleculesEquationNode.js
+++ js/common/view/MoleculesEquationNode.js
@@ -1,5 +1,6 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * Equation for the 'Molecules' and 'Game' screens. Coefficients are immutable and molecule symbols are displayed.
  *
