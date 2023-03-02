9309dde72a0fe50ed97ef03f06064bb4c8ed11ad
pixelzoom <cmalley@pixelzoom.com>
2023-03-02 08:50:28 -0700

    convert to TypeScript, https://github.com/phetsims/reactants-products-and-leftovers/issues/80

--- js/game/view/VisibilityPanel.ts
+++ js/game/view/VisibilityPanel.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Panel that contains radio buttons for selecting what's visible/hidden in Game challenges.
  * Provides the ability to hide either molecules or numbers (but not both).
@@ -8,41 +7,44 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
 import H2ONode from '../../../../nitroglycerin/js/nodes/H2ONode.js';
-import merge from '../../../../phet-core/js/merge.js';
+import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import PickOptional from '../../../../phet-core/js/types/PickOptional.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import { HBox, Node, Path, Text } from '../../../../scenery/js/imports.js';
 import eyeSlashSolidShape from '../../../../sherpa/js/fontawesome-5/eyeSlashSolidShape.js';
 import eyeSolidShape from '../../../../sherpa/js/fontawesome-5/eyeSolidShape.js';
 import AquaRadioButtonGroup from '../../../../sun/js/AquaRadioButtonGroup.js';
-import Panel from '../../../../sun/js/Panel.js';
+import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
 import RPALConstants from '../../common/RPALConstants.js';
 import reactantsProductsAndLeftovers from '../../reactantsProductsAndLeftovers.js';
 import ReactantsProductsAndLeftoversStrings from '../../ReactantsProductsAndLeftoversStrings.js';
 import GameVisibility from '../model/GameVisibility.js';
 
-// constants
 const TEXT_OPTIONS = { font: new PhetFont( 14 ) };
 const FONT_AWESOME_OPTIONS = {
   scale: 0.04,
   fill: 'black'
 };
 
+type SelfOptions = EmptySelfOptions;
+
+type VisibilityPanelOptions = SelfOptions & PickOptional<PanelOptions, 'maxWidth'>;
+
 export default class VisibilityPanel extends Panel {
 
-  /**
-   * @param {EnumerationProperty.<GameVisibility>} gameVisibilityProperty
-   * @param {Object} [options]
-   */
-  constructor( gameVisibilityProperty, options ) {
+  public constructor( gameVisibilityProperty: EnumerationProperty<GameVisibility>, providedOptions?: VisibilityPanelOptions ) {
 
-    options = merge( {
+    const options = optionize<VisibilityPanelOptions, SelfOptions, PanelOptions>()( {
+
+      // PanelOptions
       xMargin: 15,
       yMargin: 10,
       fill: 'rgb( 235, 245, 255 )',
       stroke: 'rgb( 180, 180, 180 )',
       lineWidth: 0.5
-    }, options );
+    }, providedOptions );
 
     const radioButtonItems = [
       { value: GameVisibility.SHOW_ALL, createNode: () => createShowAllNode() },
@@ -67,9 +69,8 @@ export default class VisibilityPanel extends Panel {
 
 /**
  * Creates the content for the 'Show All' radio button, an open eye with text to the right of it.
- * @returns {Node}
  */
-function createShowAllNode() {
+function createShowAllNode(): Node {
   const eyeNode = new Path( eyeSolidShape, FONT_AWESOME_OPTIONS );
   const textNode = new Text( ReactantsProductsAndLeftoversStrings.showAllStringProperty, TEXT_OPTIONS );
   return new HBox( {
@@ -81,9 +82,8 @@ function createShowAllNode() {
 /**
  * Creates the content for the 'Hide Molecules' radio button,
  * a closed eye with '123' at lower right, and text to the right.
- * @returns {Node}
  */
-function createHideMoleculesNode() {
+function createHideMoleculesNode(): Node {
   const eyeNode = new Path( eyeSlashSolidShape, FONT_AWESOME_OPTIONS );
   const moleculeNode = new Node( {
     // wrap in a Node because H2ONode doesn't work with standard options
@@ -102,9 +102,8 @@ function createHideMoleculesNode() {
 /**
  * Creates the content for the 'Hide Numbers' radio button,
  * a closed eye with H2O molecule at lower right, and text to the right.
- * @returns {Node}
  */
-function createHideNumbersNode() {
+function createHideNumbersNode(): Node {
   const eyeNode = new Path( eyeSlashSolidShape, FONT_AWESOME_OPTIONS );
   const numbersNode = new Text( '123', {
     font: new PhetFont( 8 ),
