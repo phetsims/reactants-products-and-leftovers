bae1b8f3a553116193c5ed52ee8e046480a7761b
pixelzoom <cmalley@pixelzoom.com>
2023-03-02 10:09:01 -0700

    convert to TypeScript, https://github.com/phetsims/reactants-products-and-leftovers/issues/80

--- js/molecules/view/MoleculesScreenView.ts
+++ js/molecules/view/MoleculesScreenView.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * MoleculesScreenView is the view for the 'Molecules' screen.
  *
@@ -9,31 +8,31 @@
 
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import merge from '../../../../phet-core/js/merge.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
+import Reaction from '../../common/model/Reaction.js';
 import RPALConstants from '../../common/RPALConstants.js';
 import BeforeAfterNode from '../../common/view/BeforeAfterNode.js';
 import MoleculesEquationNode from '../../common/view/MoleculesEquationNode.js';
-import RPALScreenView from '../../common/view/RPALScreenView.js';
+import RPALScreenView, { CreateBeforeAfterNodeFunction } from '../../common/view/RPALScreenView.js';
 import reactantsProductsAndLeftovers from '../../reactantsProductsAndLeftovers.js';
+import MoleculesModel from '../model/MoleculesModel.js';
 
 export default class MoleculesScreenView extends RPALScreenView {
 
-  /**
-   * @param {MoleculesModel} model
-   * @param {Tandem} tandem
-   */
-  constructor( model, tandem ) {
+  public constructor( model: MoleculesModel, tandem: Tandem ) {
 
     // Creates an equation for a specified reaction.
-    const createEquationNode = reaction => new MoleculesEquationNode( reaction );
+    const createEquationNode = ( reaction: Reaction ) => new MoleculesEquationNode( reaction );
 
     // Creates the Before/After interface for a specified reaction.
-    const createBeforeAfterNode = ( reaction, beforeExpandedProperty, afterExpandedProperty, options ) => {
-      return new BeforeAfterNode( reaction, beforeExpandedProperty, afterExpandedProperty,
-        merge( {}, options, {
-          contentSize: RPALConstants.MOLECULES_BEFORE_AFTER_BOX_SIZE,
-          minIconSize: new Dimension2( 30, 25 ) // eyeballed
-        } ) );
-    };
+    const createBeforeAfterNode: CreateBeforeAfterNodeFunction =
+      ( reaction, beforeExpandedProperty, afterExpandedProperty, options ) => {
+        return new BeforeAfterNode( reaction, beforeExpandedProperty, afterExpandedProperty,
+          merge( {}, options, {
+            contentSize: RPALConstants.MOLECULES_BEFORE_AFTER_BOX_SIZE,
+            minIconSize: new Dimension2( 30, 25 ) // eyeballed
+          } ) );
+      };
 
     super( model, createEquationNode, createBeforeAfterNode, tandem );
   }
--- js/sandwiches/view/SandwichesScreenView.ts
+++ js/sandwiches/view/SandwichesScreenView.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * SandwichesScreenView is the view for the 'Sandwiches' screen.
  *
@@ -9,21 +8,20 @@
 
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import merge from '../../../../phet-core/js/merge.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
 import RPALConstants from '../../common/RPALConstants.js';
 import BeforeAfterNode from '../../common/view/BeforeAfterNode.js';
-import RPALScreenView from '../../common/view/RPALScreenView.js';
+import RPALScreenView, { CreateBeforeAfterNodeFunction } from '../../common/view/RPALScreenView.js';
 import reactantsProductsAndLeftovers from '../../reactantsProductsAndLeftovers.js';
 import ReactantsProductsAndLeftoversStrings from '../../ReactantsProductsAndLeftoversStrings.js';
+import SandwichesModel from '../model/SandwichesModel.js';
 import SandwichesEquationNode from './SandwichesEquationNode.js';
 import SandwichNode from './SandwichNode.js';
+import SandwichRecipe from '../model/SandwichRecipe.js';
 
 export default class SandwichesScreenView extends RPALScreenView {
 
-  /**
-   * @param {SandwichesModel} model
-   * @param {Tandem} tandem
-   */
-  constructor( model, tandem ) {
+  public constructor( model: SandwichesModel, tandem: Tandem ) {
 
     // compute the size of the largest sandwich, used for view layout
     const maxCoefficient = RPALConstants.SANDWICH_COEFFICIENT_RANGE.max;
@@ -31,21 +29,24 @@ export default class SandwichesScreenView extends RPALScreenView {
     const maxSandwichSize = new Dimension2( maxSandwich.width, maxSandwich.height );
 
     // Creates an equation for a specified reaction.
-    const createEquationNode = reaction => new SandwichesEquationNode( reaction, maxSandwichSize );
+    const createEquationNode = ( reaction: SandwichRecipe ) => new SandwichesEquationNode( reaction, maxSandwichSize );
 
     // Creates the Before/After interface for a specified reaction.
-    const createBeforeAfterNode = ( reaction, beforeExpandedProperty, afterExpandedProperty, options ) => {
-      return new BeforeAfterNode( reaction, beforeExpandedProperty, afterExpandedProperty,
-        merge( {}, options, {
-          contentSize: RPALConstants.SANDWICHES_BEFORE_AFTER_BOX_SIZE,
-          showSymbols: false,
-          beforeTitleProperty: ReactantsProductsAndLeftoversStrings.beforeSandwichStringProperty,
-          afterTitleProperty: ReactantsProductsAndLeftoversStrings.afterSandwichStringProperty,
-          minIconSize: maxSandwichSize,
-          boxYMargin: 8 // large enough to accommodate the biggest sandwich
-        } ) );
-    };
+    const createBeforeAfterNode: CreateBeforeAfterNodeFunction =
+      ( reaction, beforeExpandedProperty, afterExpandedProperty, options ) => {
+        return new BeforeAfterNode( reaction, beforeExpandedProperty, afterExpandedProperty,
+          merge( {}, options, {
+            contentSize: RPALConstants.SANDWICHES_BEFORE_AFTER_BOX_SIZE,
+            showSymbols: false,
+            beforeTitleProperty: ReactantsProductsAndLeftoversStrings.beforeSandwichStringProperty,
+            afterTitleProperty: ReactantsProductsAndLeftoversStrings.afterSandwichStringProperty,
+            minIconSize: maxSandwichSize,
+            boxYMargin: 8 // large enough to accommodate the biggest sandwich
+          } ) );
+      };
 
+    //TODO https://github.com/phetsims/reactants-products-and-leftovers/issues/80
+    // @ts-expect-error
     super( model, createEquationNode, createBeforeAfterNode, tandem );
   }
 }
