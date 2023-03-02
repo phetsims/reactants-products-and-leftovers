bd32257272ef20fa8c525edf5dbc3e11f4781e5b
pixelzoom <cmalley@pixelzoom.com>
2023-03-02 08:51:18 -0700

    // @ts-nocheck

--- js/game/view/SettingsNode.js
+++ js/game/view/SettingsNode.js
@@ -1,5 +1,6 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * Portion of the scenegraph that corresponds to GamePhase.SETTINGS.
  * Displays a panel with controls used to configure a game (level selection, timer, ...)
