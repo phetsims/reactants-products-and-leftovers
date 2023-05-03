a2900b3c5bea77882dcdd41fb0da8532e8f156b7
pixelzoom <cmalley@pixelzoom.com>
2023-05-03 10:37:21 -0600

    remove invisible Line from StackNode, https://github.com/phetsims/reactants-products-and-leftovers/issues/78

--- js/common/view/StackNode.ts
+++ js/common/view/StackNode.ts
@@ -1,18 +1,15 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
 /**
- * StackNode is a vertical stack of Substances, built from the bottom up.
- * To improve performance:
- * - Nodes are created as needed.
- * - Nodes are never removed; they remain as children for the lifetime of this node.
- * - The visibility of nodes is adjusted to show the correct quantity of the substance.
+ * StackNode is a vertical stack of Substances, built from the bottom up. The maximum number of Nodes in the stack
+ * are created eagerly. The visibility of Nodes is then adjusted to show the correct quantity of the substance.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
-import { Line, Node, NodeOptions, NodeTranslationOptions } from '../../../../scenery/js/imports.js';
+import { Node, NodeOptions, NodeTranslationOptions } from '../../../../scenery/js/imports.js';
 import reactantsProductsAndLeftovers from '../../reactantsProductsAndLeftovers.js';
 import SubstanceIcon from './SubstanceIcon.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
@@ -38,38 +35,30 @@ export default class StackNode extends Node {
 
     const options = optionize<StackNodeOptions, SelfOptions, NodeOptions>()( {}, providedOptions );
 
-    /*
-     * This line is not visible (has no stroke), but defines the height of the stack,
-     * and ensures that the stack always has well-defined bounds, even when the stack
-     * contains zero items. All other nodes will be horizontally centered on this line.
-     */
-    const line = new Line( 0, 0, 0, height );
-
-    // Eagerly create the maximum number of nodes in the stack, positioned from the bottom up.
-    const iconNodes: SubstanceIcon[] = [];
+    // Eagerly create the maximum number of Nodes in the stack, positioned from the bottom up.
+    const icons: SubstanceIcon[] = [];
     for ( let i = 0; i < quantityProperty.range.max; i++ ) {
-      const iconNode = new SubstanceIcon( iconProperty, {
+      icons.push( new SubstanceIcon( iconProperty, {
         centerX: 0,
         centerY: startCenterY - ( i * deltaY )
-      } );
-      iconNodes.push( iconNode );
+      } ) );
     }
 
-    // Make the proper number of nodes visible in the stack, from the bottom up.
+    // Make the proper number of Nodes visible in the stack, from the bottom up.
     const quantityPropertyObserver = ( quantity: number ) => {
-      for ( let i = 0; i < iconNodes.length; i++ ) {
-        iconNodes[ i ].visible = ( i < quantity );
+      for ( let i = 0; i < icons.length; i++ ) {
+        icons[ i ].visible = ( i < quantity );
       }
     };
     quantityProperty.link( quantityPropertyObserver ); // must be unlinked in dispose
 
-    options.children = [ line, ...iconNodes ];
+    options.children = icons;
 
     super( options );
 
     this.disposeStackNode = () => {
-      iconNodes.forEach( node => node.dispose() );
-      iconNodes.length = 0;
+      icons.forEach( icon => icon.dispose() );
+      icons.length = 0;
       if ( quantityProperty.hasListener( quantityPropertyObserver ) ) {
         quantityProperty.unlink( quantityPropertyObserver );
       }
