2fa7426a69c5a71a10b486e629f6ceebdd816bb0
phet-dev <phetadmin@gmail.com>
2024-07-11 04:38:22 -0600

    update copyright dates from daily grunt work

--- js/common/view/StacksAccordionBox.ts
+++ js/common/view/StacksAccordionBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  *  Accordion box that shows stacks of substances. Used in the 'Sandwiches' and 'Molecules' screens.
