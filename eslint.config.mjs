b015fe29e80090346b67dd0b488638112a494ad5
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:08 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for reactants-products-and-leftovers.
+ * ESLint configuration for reactants-products-and-leftovers.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
