d82a8b3e076b09cb8be7d64efa0b10d1a1b84b6f
phet-dev <phetadmin@gmail.com>
2025-01-16 03:33:31 -0700

    update copyright dates from daily grunt work

--- js/common/view/ReactionBarNode.ts
+++ js/common/view/ReactionBarNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2024, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * ReactionBarNode is the horizontal bar that appears at the top of the screen. It contains radio buttons for selecting
