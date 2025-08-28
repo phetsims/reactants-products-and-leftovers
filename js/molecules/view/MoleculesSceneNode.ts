b92ab4dc5c90735c08282a59098300c7dac8fe59
phet-dev <phetadmin@gmail.com>
2025-08-28 04:37:41 -0600

    update copyright dates from daily grunt work

--- js/molecules/view/MoleculesSceneNode.ts
+++ js/molecules/view/MoleculesSceneNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2023-2025, University of Colorado Boulder
 
 /**
  * MoleculesSceneNode is the scene for one reaction in the 'Molecules' screen.
--- js/sandwiches/view/SandwichesSceneNode.ts
+++ js/sandwiches/view/SandwichesSceneNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2023-2025, University of Colorado Boulder
 
 /**
  * SandwichesSceneNode is the scene for one sandwich recipe (reaction) in the 'Sandwiches' screen.
